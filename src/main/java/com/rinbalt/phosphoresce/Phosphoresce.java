07789d5df8b58148ffcd41a6178bcd270ae83e97
Candlemancer <Candlemancer@gmail.com>
2014-06-21 09:01:06 -0600

    Initial Commit

---
+++ src/main/java/com/rinbalt/phosphoresce/Phosphoresce.java
@@ -0,0 +1,31 @@
+package com.rinbalt.phosphoresce;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "Phosphoresce", name = "Phosphoresce", version = "1.7.2-0.1a")
+public class Phosphoresce {
+
+    @Mod.Instance("Phosphoresce")
+    public static Phosphoresce insance;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent preInitEvent) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent initEvent) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent postInitEvent) {
+
+    }
+
+
+}
